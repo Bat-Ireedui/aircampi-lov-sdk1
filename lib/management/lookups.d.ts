fedf1655a3b0b346ff051e152db197e68bfd47e2
dizu00 <86697441+Bat-Ireedui@users.noreply.github.com>
2022-11-04 14:19:15 +0800

    Setup Package

--- lib/management/lookups.d.ts
+++ lib/management/lookups.d.ts
@@ -2,11 +2,11 @@ import { ILookups, LookupView, LookupData, Lookups } from "./schema";
 import { Base } from ".";
 export declare class Lookup extends Base implements ILookups {
     getLookups(): Promise<Lookups>;
-    getLookupById(tenant_id: string): Promise<LookupView>;
-    updateLookup(tenant_id: string, body: LookupData): Promise<LookupView>;
+    getLookupById(lookup_code: string): Promise<LookupView>;
+    updateLookup(lookup_code: string, body: LookupData): Promise<LookupView>;
     createLookup(body: {
         code: string;
         name: string;
     } & LookupData): Promise<LookupView>;
-    deleteLookup(tenant_id: string): Promise<LookupView>;
+    deleteLookup(lookup_code: string): Promise<LookupView>;
 }
